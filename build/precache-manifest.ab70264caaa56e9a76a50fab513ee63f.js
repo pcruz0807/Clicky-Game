7eff9be467bc73774976788883f8f63e730b82a1
pcruz0807 <codecruz86@gmail.com>
2019-05-04 18:54:07 -0700

    trying to get this to work with react

---
+++ build/precache-manifest.ab70264caaa56e9a76a50fab513ee63f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f99484c5013618633aee3382259f1e",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4a6899234c6f4df73689",
+    "url": "/Clicky-Game/static/css/main.d42bf1c8.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "4a6899234c6f4df73689",
+    "url": "/Clicky-Game/static/js/main.278b2e2b.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
